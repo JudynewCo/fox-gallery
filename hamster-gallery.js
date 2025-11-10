4b8532922397ea635cd735899a481d39a0e7247c
“jubileejost” <juxi23wl@student.ju.se>
2025-11-09 23:25:09 -0500

    push

--- hamster-gallery.js
+++ hamster-gallery.js
@@ -26,7 +26,7 @@ export class HamsterGallery extends DDDSuper(I18NMixin(LitElement)) {
   }
 
   async loadData() {
-    const res = await fetch("./hamster.json");
+    const res = await fetch(new URL("./hamster.json", import.meta.url));
     const data = await res.json();
 
     this.users = data.users.map((user) => ({
